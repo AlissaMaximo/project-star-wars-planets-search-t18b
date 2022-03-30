75c8e86465613ce3c9f4715c66313ebcdaf716f2
AlissaMaximo <alissahiratamaximo@gmail.com>
2022-03-30 13:01:09 -0300

    useContext implementado para plaentas.

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,47 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Context from '../context/MyContext';
 
-function Table(/* aqui vem o valor do context */) {
+function Table() {
+  const { planets } = useContext(Context);
+
   return (
     <>
       <p>oi do Table temporário</p>
-      <Context.Consumer>
-        {
-          (context) => (
-            <>
-              <p>
-                oi do context.consumer. context:
-                {' '}
-                { context }
-              </p>
-              <table>
-                <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>Rotation Period</th>
-                    <th>Orbital Period</th>
-                    <th>Diameter</th>
-                    <th>Climate</th>
-                    <th>Gravity</th>
-                    <th>Terrain</th>
-                    <th>Surface Water</th>
-                    <th>Population</th>
-                    <th>Films</th>
-                    <th>Created</th>
-                    <th>Edited</th>
-                    <th>URL</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {
-                    //aqui entra o map com os <tr> <td></td> </tr>
-                  }
-                </tbody>
-              </table>
-            </>
-          )
-        }
-      </Context.Consumer>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            console.log(planets)
+            // aqui entra o map com os <tr> <td></td> </tr>
+          }
+        </tbody>
+      </table>
     </>
   );
 }
