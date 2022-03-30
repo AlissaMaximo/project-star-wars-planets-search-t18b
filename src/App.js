ac9644fe8668c36163fd87ed023f3e89605107b3
AlissaMaximo <alissahiratamaximo@gmail.com>
2022-03-30 16:09:17 -0300

    Criado o input do requisito 2.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import NameInput from './components/NameInput';
 import Table from './components/Table';
 
 function App() {
   return (
-    <Table />
+    <>
+      <NameInput />
+      <Table />
+    </>
   );
 }
 
---
+++ src/components/NameInput.js
@@ -0,0 +1,10 @@
+import React, { useContext } from 'react';
+import context from '../context/MyContext';
+
+function NameInput() {
+  return (
+    <input type="text" />
+  );
+}
+
+export default NameInput;
